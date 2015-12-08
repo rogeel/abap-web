30e3825d062ff392b29d78d3fb209c7ea0b3f0f6
Roger Perez <roger@takum.co>
2015-12-08 12:38:33 -0500

    work in contact form validation

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,5 +4,31 @@
 	{
 		$('.bxslider').bxSlider();
 		$(".flexnav").flexNav();
+
+		$('#contact-form').validate({
+	        lang: 'es',
+	        
+
+	        errorPlacement: function(error, element) {
+	            
+	               error.insertAfter($(element));
+	            
+	        },
+
+	        submitHandler: function(form) {
+	            
+
+	            
+	            
+	        },invalidHandler: function(form, validator) {
+	            
+	        }
+	    });
 	})
+
+
+
+    
+
+
 })(jQuery);
